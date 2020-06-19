b7e1f42cf45404074e6ac17c743290c707d74af7
deim0707 <dima1parshin@gmail.com>
2020-06-19 20:52:59 +0300

    добавил сборку

---
+++ build/precache-manifest.91282c753db2f3fe23664badc2356b06.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8160c30e541ef235a3d8f58cf5fbd5eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c461538605443ababb1e",
+    "url": "./static/css/main.1efbd986.chunk.css"
+  },
+  {
+    "revision": "fb00de60537fc67cf841",
+    "url": "./static/js/2.65f88666.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.65f88666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aff6dc47aa7822ab60c",
+    "url": "./static/js/3.6640dbf0.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "./static/js/3.6640dbf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c461538605443ababb1e",
+    "url": "./static/js/main.ac6c1981.chunk.js"
+  },
+  {
+    "revision": "3365b7baf7c4a85c2747",
+    "url": "./static/js/runtime-main.eddff80b.js"
+  },
+  {
+    "revision": "f3fc628d9c18c4021127b186883a2823",
+    "url": "./static/media/birds.f3fc628d.mp3"
+  },
+  {
+    "revision": "6f99b1c446eed8bf32e5b350542ed1bb",
+    "url": "./static/media/bonfire1.6f99b1c4.mp3"
+  },
+  {
+    "revision": "00d47881645cb7059791d1027c16bbf1",
+    "url": "./static/media/forest2.00d47881.mp3"
+  },
+  {
+    "revision": "e7efd03b6ff22cc49b572ebccc209358",
+    "url": "./static/media/sea_birds.e7efd03b.mp3"
+  },
+  {
+    "revision": "ef62803f124f36f61f142665a063611c",
+    "url": "./static/media/south_forest.ef62803f.mp3"
+  },
+  {
+    "revision": "d06ce2ed2854e726b03431c1d3e5c95d",
+    "url": "./static/media/thunderstorm2.d06ce2ed.mp3"
+  },
+  {
+    "revision": "5ddf0ee73bd9b8b51f5fce3a4e9f1739",
+    "url": "./static/media/train.5ddf0ee7.mp3"
+  },
+  {
+    "revision": "07f17009c4ea89fa9423262157413de1",
+    "url": "./static/media/wind.07f17009.mp3"
+  }
+]);
