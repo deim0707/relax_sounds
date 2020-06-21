d867f8f20a9f9151d317b7d7cc33a0b20db6879a
svetla esina <magistresina@gmail.com>
2020-06-21 21:42:04 +0300

    Code clean. Add classnames. Fix random effect

--- src/components/soundItem/soundItem.js
+++ src/components/soundItem/soundItem.js
@@ -5,94 +5,86 @@ import useSound from "use-sound";
 import './soundItem.css'
 import items from "./items";
 
-
 function SoundItem({type}) {
     const dispatch = useDispatch();
-    const pauseAll = useSelector((state) => state.pauseAll);
+    const isPauseAll = useSelector((state) => state.pauseAll);
     const isRandom = useSelector((state) => state.random);
-    const isStoped = useSelector((state) => state.stopAll);
-
+    const isStopped = useSelector((state) => state.stopAll);
 
     const item = items[type]; //here sound and image
 
     const [volume, setVolume] = useState(0.7);
-    const [playing, setPlaying] = useState(false);
+    const [isPlaying, setPlaying] = useState(false);
 
-    const [play, {pause}] = useSound(
-        item.sound,
-        {
-            volume: volume,
-            loop: true,
-        }
-    );
+    const [play, {pause}] = useSound(item.sound, {
+        volume: volume,
+        loop: true,
+    });
 
     //pause all
     useEffect(() => {
-        if (pauseAll && playing) pause();
-        if (!pauseAll && playing) play();
-    }, [pauseAll]);
+        if (isPlaying) {
+            if (isPauseAll) pause();
+            else play();
+        }
+    }, [isPauseAll]);
 
     //random
     useEffect(() => {
-        if (isRandom && Math.random() > 0.5) {
-            play();
-            setPlaying(true);
-            dispatch(random(false))
-        }
-        if (isRandom && Math.random() < 0.5) {
-            pause();
-            setPlaying(false);
-            dispatch(random(false))
+        if (isRandom) {
+            dispatch(random(false));
+
+            if (Math.random() > 0.5) {
+                playSound();
+            } else {
+                stopSound();
+            }
         }
     }, [isRandom]);
 
     //stoped all
     useEffect(() => {
-        if (isStoped) {
-            pause()
-            setPlaying(false);
-            dispatch(stopAll(false))
+        if (isStopped) {
+            stopSound();
+            dispatch(stopAll(false));
         }
-    }, [isStoped])
+    }, [isStopped]);
 
-    function playPauseSound() {
-        if (playing) {
-            setPlaying(false);
-            pause()
-        } else {
-            setPlaying(true);
-            play()
-        }
-
+    function stopSound() {
+        pause();
+        setPlaying(false);
     }
 
+    function playSound() {
+        play();
+        setPlaying(true);
+    }
 
-    const form = <form>
-        <input
-            type="range"
-            className="volume-range"
-            id="formControlRange"
-            min='10'
-            max='100'
-            defaultValue='70'
-            onChange={(e) => {
-                setVolume((e.target.value / 100))
-            }}
-        />
-    </form>;
+    function playPauseSound() {
+        if (isPlaying) {
+            stopSound();
+        } else {
+            playSound();
+        }
+    }
+
+    const volumeControl = <input
+        type="range"
+        className="volume-range"
+        id="formControlRange"
+        min='10'
+        max='100'
+        defaultValue='70'
+        onChange={(e) => setVolume((e.target.value / 100))}
+    />;
 
     return (
-        <div
-            className="sound-item"
-            style={playing ? {fill: 'green', color: 'green'} : {fill: 'black', color: 'black'}}
-        >
-            <div onClick={playPauseSound}>
+        <div className={`sound-item sound-${isPlaying ? 'on' : 'off'}`}>
+            <div role="button" onClick={playPauseSound}>
                 {item.img}
             </div>
-
-            {playing ? form : null}
+            {isPlaying && volumeControl}
         </div>
-
     );
 }
 
